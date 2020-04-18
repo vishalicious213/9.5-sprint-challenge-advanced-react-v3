a4469bd53e8282338c636ea5e31c3ab92f1ba0cc
vishalicious213 <vishalicious213@gmail.com>
2020-04-17 20:33:03 -0400

    style page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import GetPlayers from './components/GetPlayers';
 
 function App() {
   return (
     <div className="App">
-
+      <GetPlayers />
     </div>
   );
 }
--- client/src/components/GetPlayers.js
+++ client/src/components/GetPlayers.js
@@ -14,7 +14,7 @@ class GetPlayers extends React.Component {
       axios
         .get(`http://localhost:5000/api/players`)
         .then(results => {
-          console.log('Results ', results.data);
+        //   console.log('Results ', results.data);
           this.setState({players: results.data}); // don't run in render() - creates loop
         })
         .catch(error => console.log('Error: ', error));
@@ -29,10 +29,12 @@ class GetPlayers extends React.Component {
         return (
             <section>
                 <header>
+                    <img className='fifaLogo' src='https://1000logos.net/wp-content/uploads/2017/01/FIFA-Logo-500x381.png' alt='FIFA logo'></img>
                     <div className='App-header'>
                         <h2 className='title'>Women's World Cup</h2>
                         <div className='subtitle'>2019 Player Search Rankings</div>
                     </div>
+                    <img className='womensCup' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/2019_FIFA_Women%27s_World_Cup.svg/800px-2019_FIFA_Women%27s_World_Cup.svg.png' alt='2019 Womens World Cup logo'></img>
                 </header>
                 <DisplayPlayers players={this.state.players} />
             </section>
